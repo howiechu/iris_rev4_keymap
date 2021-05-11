17a89638b72dfc00a831c6ca9176d17d55ae5492
HNC <howie.chu@protonmail.com>
2021-05-11 07:36:06 -0400

    first commit

---
+++ config.h
@@ -0,0 +1,38 @@
+/*
+Copyright 2017 Danny Nguyen <danny@keeb.io>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// #define USE_I2C
+#define EE_HANDS
+
+#undef RGBLED_NUM
+// #define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLED_NUM 12
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+
+#define TAPPING_TOGGLE 2
+
+#define TAP_CODE_DELAY 10
+
+#define ENCODER_RESOLUTION 4
+
+#define MOUSEKEY_WHEEL_DELAY 150
+#define MOUSEKEY_WHEEL_INTERVAL 80
